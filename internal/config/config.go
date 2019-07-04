1e46808ac5bbe0376d54580e4797b79c96a291e8
Mikhail Kalinin <objque@gmail.com>
2019-07-04 22:30:34 +0300

    Update config

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,13 +11,11 @@ import (
 var Config *AppConfig
 
 type AppConfig struct {
-	HTTP     HTTPConfig        `yaml:"http"`
-	DB       DBConfig          `yaml:"db"`
-	Log      LogConfig         `yaml:"log"`
-	Fetching Fetching          `yaml:"fetching"`
-	Stores   map[string]*Store `yaml:"stores"`
-	Notifier Notifier          `yaml:"notifier"`
-	Sentry   Sentry            `yaml:"sentry"`
+	HTTP     HTTPConfig `yaml:"http"`
+	DB       DBConfig   `yaml:"db"`
+	Log      LogConfig  `yaml:"log"`
+	Notifier Notifier   `yaml:"notifier"`
+	Sentry   Sentry     `yaml:"sentry"`
 }
 
 type HTTPConfig struct {
@@ -31,30 +29,13 @@ type LogConfig struct {
 }
 
 type DBConfig struct {
-	DBType  string `yaml:"dbtype"`
-	DBHost  string `yaml:"dbhost"`
-	DBName  string `yaml:"dbname"`
-	DBLogin string `yaml:"dblogin"`
-	DBPass  string `yaml:"dbpass"`
-	Log     bool   `yaml:"log"`
+	Type  string `yaml:"type"`
+	Host  string `yaml:"host"`
+	Name  string `yaml:"name"`
+	Login string `yaml:"login"`
+	Pass  string `yaml:"pass"`
+	Log   bool   `yaml:"log"`
 }
-
-type Fetching struct {
-	RefetchAfterHours   float64 `yaml:"refetch_after_hours"`
-	CountOfSkippedHours float64 `yaml:"count_of_skipped_hours"`
-}
-
-type Store struct {
-	URL          string `yaml:"url"`
-	FetchWorkers int    `yaml:"fetch_workers"`
-	Meta         Meta   `yaml:"meta"`
-	ReleaseURL   string `yaml:"release_url"`
-	ArtistURL    string `yaml:"artist_url"`
-	Name         string `yaml:"name"`
-	Fetch        bool   `json:"fetch"`
-}
-
-type Meta map[string]string
 
 type Notifier struct {
 	TelegramToken       string  `yaml:"telegram_token"`
@@ -91,14 +72,11 @@ func Load(data []byte) error {
 }
 
 func (db *DBConfig) GetConnString() (dialect, connString string) {
-	if db.DBType != "mysql" {
+	if db.Type != "mysql" {
 		panic("Only mysql is currently supported")
 	}
 	connString = fmt.Sprintf(
 		"%v:%v@tcp(%v)/%v?charset=utf8&parseTime=True&loc=UTC",
-		db.DBLogin,
-		db.DBPass,
-		db.DBHost,
-		db.DBName)
-	return db.DBType, connString
+		db.Login, db.Pass, db.Host, db.Name)
+	return db.Type, connString
 }
--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -8,37 +8,48 @@ import (
 
 func TestConfig_Load(t *testing.T) {
 	err := Load([]byte(`
-stores:
-    yandex:
-        url: https://music.yandex.ru
-        fetch_workers: 5
-        name: "Yandex.Music"
-        fetch: false
+http:
+    port: 9933
 
-    itunes:
-        url: https://api.music.apple.com
-        fetch_workers: 25
-        name: "Apple Music"
-        fetch: true
-        meta:
-          token: "dd214b951ab64de0af62d53678750c90"
-          region: "us"
+db:
+    type:  'mysql'
+    host:  'mariadb'
+    name:  'notify'
+    login: 'notify'
+    pass:  'notify'
+    log: false
+
+log:
+    level: DEBUG
+    file: 'notify.log'
+
+notifier:
+    count_of_skipped_hours: 8
+    telegram_token: "12340255:BBBZZZJJJJJAAAEEEEE"
+
+sentry:
+  enabled: true
+  key: "https://xxxxx:yyyyy@sentry.io/123456"
 `))
 
 	assert.NoError(t, err)
-	assert.Len(t, Config.Stores, 2)
 
-	assert.Equal(t, "https://music.yandex.ru", Config.Stores["yandex"].URL)
-	assert.Equal(t, 5, Config.Stores["yandex"].FetchWorkers)
-	assert.Equal(t, "Yandex.Music", Config.Stores["yandex"].Name)
-	assert.False(t, Config.Stores["yandex"].Fetch)
+	assert.Equal(t, "", Config.HTTP.IP)
+	assert.Equal(t, 9933, Config.HTTP.Port)
 
-	assert.Equal(t, "https://api.music.apple.com", Config.Stores["itunes"].URL)
-	assert.Equal(t, 25, Config.Stores["itunes"].FetchWorkers)
-	assert.Len(t, Config.Stores["itunes"].Meta, 2)
-	assert.Equal(t, "dd214b951ab64de0af62d53678750c90", Config.Stores["itunes"].Meta["token"])
-	assert.Equal(t, "us", Config.Stores["itunes"].Meta["region"])
-	assert.Equal(t, "us", Config.Stores["itunes"].Meta["region"])
-	assert.Equal(t, "Apple Music", Config.Stores["itunes"].Name)
-	assert.True(t, Config.Stores["itunes"].Fetch)
+	assert.Equal(t, "mysql", Config.DB.Type)
+	assert.Equal(t, "mariadb", Config.DB.Host)
+	assert.Equal(t, "notify", Config.DB.Name)
+	assert.Equal(t, "notify", Config.DB.Login)
+	assert.Equal(t, "notify", Config.DB.Pass)
+	assert.False(t, Config.DB.Log)
+
+	assert.Equal(t, "DEBUG", Config.Log.Level)
+	assert.Equal(t, "notify.log", Config.Log.File)
+
+	assert.Equal(t, float64(8), Config.Notifier.CountOfSkippedHours)
+	assert.Equal(t, "12340255:BBBZZZJJJJJAAAEEEEE", Config.Notifier.TelegramToken)
+
+	assert.True(t, Config.Sentry.Enabled)
+	assert.Equal(t, "https://xxxxx:yyyyy@sentry.io/123456", Config.Sentry.Key)
 }
