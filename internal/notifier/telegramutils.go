6a8b5c81a4bad49c3c7e4a71db8f184d81e4b020
Mikhail Kalinin <objque@gmail.com>
2019-07-30 17:08:24 +0300

    Replace open with listen on

--- internal/notifier/telegramutils.go
+++ internal/notifier/telegramutils.go
@@ -24,7 +24,7 @@ func makeText(artistName string, release *releases.Release) string {
 func makeButtons(release *releases.Release) *[][]tgbotapi.InlineKeyboardButton {
 	buttons := [][]tgbotapi.InlineKeyboardButton{}
 	storeDetails := config.Config.Stores[release.StoreName]
-	buttonLabel := fmt.Sprintf("Open in %s", storeDetails.Name)
+	buttonLabel := fmt.Sprintf("Listen on %s", storeDetails.Name)
 	url := fmt.Sprintf(config.Config.Stores[release.StoreName].ReleaseURL, release.StoreID)
 	buttons = append(buttons, tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonURL(buttonLabel, url)))
 	return &buttons
